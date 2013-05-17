6b914c0a17cfdc8e153fe68313ee2d1aadf369e8
Tiramisu 1993 <sabergeass@gmail.com>
2013-05-17 11:27:57 +0800

    Update mezzanine_install.py

--- mezzanine_install.py
+++ mezzanine_install.py
@@ -1,15 +1,15 @@
 import os 
 #import shutil
-def pre_install(path_flup):
+def pre_install(path_flup):        #the function to install the pre_environment about mezzanine and nginx and fstcgi 
 	try:
 		os.system('sudo apt-get install  python-dev python-setuptools python-imaging build-essential')
 		os.system('pip install south')
 		os.system('pip install django-compressor')
 		os.system('pip install mezzanine')
 		os.system('sudo apt-get install nginx')
-	        if os.path.exists(path_flup):
+	        if os.path.exists(path_flup):     
         		os.chdir(path_flup)
-        		install_flup()
+        		install_flup()      #call function about flup install
 	        else:
 	        	os.mkdir(path_flup)
   		     	os.chdir(path_flup)
@@ -18,8 +18,8 @@ def pre_install(path_flup):
 	except:	
 		state = 'preinstall_error'
 	return state
-
-def install_flup():
+#flup is necessary component with fastcgi,It can pass paraments from Django to nginx 
+def install_flup():              #the function to install flup
 	try:
 		os.system('sudo wget  http://www.saddi.com/software/flup/dist/flup-1.0.2.tar.gz')
 	except:
